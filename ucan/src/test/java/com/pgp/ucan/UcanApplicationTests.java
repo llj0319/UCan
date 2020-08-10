b909363537364ca7e923242a1e301f0ef0751a29
llj0319 <llj0319a@163.com>
2020-08-10 15:09:04 +0800

    new project

---
+++ ucan/src/test/java/com/pgp/ucan/UcanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgp.ucan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
